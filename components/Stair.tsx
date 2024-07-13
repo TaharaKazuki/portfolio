8f5639a52c1655721c9380adfa148b36be474287
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-07-13 13:41:52 +0900

    updaete

--- components/Stair.tsx
+++ components/Stair.tsx
@@ -1,45 +1,70 @@
 import { motion } from 'framer-motion';
 
-import { cn } from '@/lib/utils';
-
-const stairAnimation = {
+const diagonalLineAnimation = {
   initial: {
-    top: '0%',
+    scaleX: 0,
+    rotate: 45,
+    x: '-50%',
   },
   animate: {
-    top: '100%',
-  },
-  exit: {
-    top: ['100%', '0%'],
+    scaleX: 1,
+    transition: {
+      duration: 1,
+      ease: 'easeInOut',
+    },
   },
 };
 
-const STEP = 7;
+const splitAnimationLeft = {
+  initial: {
+    x: 0,
+  },
+  animate: {
+    x: '-100%',
+    transition: {
+      duration: 1,
+      ease: 'easeInOut',
+      delay: 1,
+    },
+  },
+};
 
-const reverseIndex = (index: number) => {
-  const totalStep = STEP;
-  return totalStep - index - 1;
+const splitAnimationRight = {
+  initial: {
+    x: 0,
+  },
+  animate: {
+    x: '100%',
+    transition: {
+      duration: 1,
+      ease: 'easeInOut',
+      delay: 1,
+    },
+  },
 };
 
 const Stairs = () => {
   return (
-    <>
-      {[...Array(STEP)].map((_, i) => (
-        <motion.div
-          key={i}
-          variants={stairAnimation}
-          initial="initial"
-          animate="animate"
-          exit="exit"
-          transition={{
-            duration: 0.4,
-            ease: 'easeInOut',
-            delay: reverseIndex(i) * 0.1,
-          }}
-          className={cn('relative size-full bg-white')}
-        />
-      ))}
-    </>
+    <div className="relative h-screen w-full overflow-hidden">
+      <motion.div
+        className="absolute left-1/2 top-1/2 h-[2px] origin-center bg-black"
+        variants={diagonalLineAnimation}
+        initial="initial"
+        animate="animate"
+      />
+      <motion.div
+        className="absolute left-0 top-0 h-full w-1/2 bg-white"
+        variants={splitAnimationLeft}
+        initial="initial"
+        animate="animate"
+      />
+      <motion.div
+        className="absolute right-0 top-0 h-full w-1/2 bg-white"
+        variants={splitAnimationRight}
+        initial="initial"
+        animate="animate"
+      />
+    </div>
   );
 };
 
