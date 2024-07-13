40ef646fe22396aae98c0313596ff5587acd3a18
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-07-13 16:32:36 +0900

    update

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,10 +1,13 @@
 'use client';
 
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { CiMenuFries } from 'react-icons/ci';
 
 import Logo from './Logo';
 import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
+import { LINKS } from '@/app/_const';
+import { cn } from '@/lib/utils';
 
 const MobileNav = () => {
   const pathname = usePathname();
@@ -18,6 +21,33 @@ const MobileNav = () => {
         <div className="mb-40 mt-32 text-center text-2xl">
           <Logo />
         </div>
+
+        <nav className="flex flex-col items-center justify-center gap-8">
+          {LINKS.map((link, i) => {
+            const isActive = link.path === pathname;
+            return (
+              <Link href={link.path} key={i} className="group relative">
+                <span
+                  className={cn(
+                    'font-medium capitalize transition-all',
+                    isActive ? 'text-accent' : 'group-hover:text-accent'
+                  )}
+                >
+                  {link.name}
+                </span>
+                <span
+                  className={cn(
+                    'absolute bottom-0 h-[2px] bg-accent transition-all duration-300 neon-accent',
+                    isActive
+                      ? 'left-0 w-full'
+                      : 'left-1/2 w-0 group-hover:left-0 group-hover:w-full group-hover:neon-accent'
+                  )}
+                  style={{ transformOrigin: 'center' }}
+                ></span>
+              </Link>
+            );
+          })}
+        </nav>
       </SheetContent>
     </Sheet>
   );
