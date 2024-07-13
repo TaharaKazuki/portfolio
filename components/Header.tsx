ca9232ecf8157f328c59aabb0fcb0000272a99a0
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-07-13 09:49:00 +0900

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,14 +7,14 @@ import Nav from './ui/Nav';
 const Header = () => {
   return (
     <header className="py-8 text-white xl:py-12">
-      <div className="container mx-auto">
+      <div className="container mx-auto flex items-center justify-between">
         <Link href="/">
           <h1 className="text-4xl font-semibold">
             DK<span className="text-accent">.</span>
           </h1>
         </Link>
 
-        <div className="hidden xl:flex">
+        <div className="hidden items-center gap-8 xl:flex">
           <Nav />
           <Link href="/contact">
             <Button>Hi Contact with Me</Button>
--- components/ui/Nav.tsx
+++ components/ui/Nav.tsx
@@ -1,7 +1,47 @@
-import React from 'react';
+'use client';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import { cn } from '@/lib/utils';
+
+const LINKS = [
+  { path: '/', name: 'home' },
+  { path: '/services', name: 'services' },
+  { path: '/resume', name: 'resume' },
+  { path: '/work', name: 'work' },
+];
 
 const Nav = () => {
-  return <div>Nav</div>;
+  const pathname = usePathname();
+
+  return (
+    <nav className="flex gap-8">
+      {LINKS.map((link, i) => {
+        const isActive = link.path === pathname;
+        return (
+          <Link href={link.path} key={i} className="group relative">
+            <span
+              className={cn(
+                'font-medium capitalize transition-all',
+                isActive ? 'text-accent' : 'group-hover:text-accent'
+              )}
+            >
+              {link.name}
+            </span>
+            <span
+              className={cn(
+                'absolute bottom-0 h-[2px] bg-accent transition-all duration-300',
+                isActive
+                  ? 'left-0 w-full'
+                  : 'left-1/2 w-0 group-hover:left-0 group-hover:w-full'
+              )}
+              style={{ transformOrigin: 'center' }}
+            ></span>
+          </Link>
+        );
+      })}
+    </nav>
+  );
 };
 
 export default Nav;
