ae079acf0ed0fe8cf1fb6883eeaae126c33f05c7
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-07-13 10:39:27 +0900

    update

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const page = () => {
-  return <div>page</div>;
+const ServicePage = () => {
+  return <div>Service</div>;
 };
 
-export default page;
+export default ServicePage;
