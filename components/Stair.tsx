54d74e67981030aa606fb44f90fa9435e1713717
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-07-13 15:18:55 +0900

    update

--- components/Stair.tsx
+++ components/Stair.tsx
@@ -1,6 +1,5 @@
 import { motion } from 'framer-motion';
 
-// variants
 const stairAnimation = {
   initial: {
     top: '0%',
@@ -13,7 +12,7 @@ const stairAnimation = {
   },
 };
 
-const STEP = 8;
+const STEP = 10;
 const reverseIndex = (index: number) => {
   const totalSteps = STEP;
   return totalSteps - index - 1;
@@ -35,8 +34,12 @@ const Stairs = () => {
               ease: 'easeInOut',
               delay: reverseIndex(index) * 0.1,
             }}
-            className="relative size-full neon-accent"
-          />
+            className="relative flex size-full flex-row gap-2 p-16"
+          >
+            <div className="h-full w-2 rounded-full bg-accent neon-accent" />
+            <div className="h-full w-2 rounded-full bg-accent neon-accent" />
+            <div className="h-full w-2 rounded-full bg-accent neon-accent" />
+          </motion.div>
         );
       })}
     </>
