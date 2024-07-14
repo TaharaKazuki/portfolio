c1a62e0891bc58191c0da48af335bf28afc3faf1
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-07-14 16:33:40 +0900

    updaete

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import React from 'react';
 
+import { cn } from '@/lib/utils';
+
 type LogoProps = {
   onClickHandler?: () => void;
 };
@@ -8,11 +10,21 @@ type LogoProps = {
 const Logo = ({ onClickHandler }: LogoProps) => {
   return (
     <>
-      <Link href="/" onClick={onClickHandler}>
-        <h1 className="text-4xl font-semibold">
+      <Link href="" className="group relative">
+        <h1
+          className={cn(
+            'capitalize transition-all group-hover:text-neon-accent text-4xl font-semibold duration-300 xl:hover:text-neon-accent'
+          )}
+        >
           DK
           <span className="ml-2 inline-block size-2 rounded-full bg-accent neon-accent" />
         </h1>
+        <span
+          className={cn(
+            'absolute bottom-0 h-[4px] bg-accent transition-all duration-300 neon-accent left-1/2 w-0 group-hover:left-0 group-hover:w-full group-hover:neon-accent'
+          )}
+          style={{ transformOrigin: 'center' }}
+        />
       </Link>
     </>
   );
