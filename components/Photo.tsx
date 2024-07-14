bf4ca97f6ebb3f60674a0aafb2783ef334b82d1c
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-07-14 10:52:44 +0900

    update

--- components/Photo.tsx
+++ components/Photo.tsx
@@ -20,7 +20,7 @@ const Photo = () => {
             opacity: 1,
             transition: { delay: 2.4, duration: 0.4, ease: 'easeInOut' },
           }}
-          className="absolute size-[280px] mix-blend-lighten xl:size-[480px]"
+          className="absolute left-3 size-[280px] mix-blend-lighten xl:size-[480px]"
         >
           <Image
             src="/assets/photo.png"
@@ -32,17 +32,37 @@ const Photo = () => {
           />
         </motion.div>
         <motion.svg
-          className="size-[300px] xl:size-[506px]"
+          className="size-[320px] xl:size-[526px]"
           fill="transparent"
-          viewBox="0 0 506 506"
+          viewBox="0 0 520 520"
           xmlns="http://www.w3.org/2000/svg"
         >
+          {/* 外側の円 */}
+          <motion.circle
+            cx="253"
+            cy="253"
+            r="240"
+            stroke="#00ff99"
+            strokeWidth="3"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            initial={{ strokeDasharray: '24 10 0 0' }}
+            animate={{
+              strokeDasharray: ['15 120 25 25', '16 25 92 72', '4 250 22 22'],
+              rotate: [120, 360],
+            }}
+            transition={{
+              duration: 20,
+              repeat: Infinity,
+              repeatType: 'reverse',
+            }}
+          />
           <motion.circle
             cx="253"
             cy="253"
             r="250"
-            stroke="#00ff99"
-            strokeWidth="2"
+            stroke="#025D31"
+            strokeWidth="3"
             strokeLinecap="round"
             strokeLinejoin="round"
             initial={{ strokeDasharray: '24 10 0 0' }}
