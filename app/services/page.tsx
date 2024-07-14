ef0b61d6371870e3987d26e68982b12b98cdea89
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-07-14 19:29:41 +0900

    update

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -2,12 +2,12 @@
 
 import { motion } from 'framer-motion';
 import { FaMobileAlt, FaPalette, FaHandHoldingHeart } from 'react-icons/fa';
-import { SiTypescript } from 'react-icons/si';
+import { SiNextdotjs } from 'react-icons/si';
 
 const SERVICES_INFO = [
   {
     icon: (
-      <SiTypescript className="text-white duration-500 group-hover:text-accent" />
+      <SiNextdotjs className="text-white duration-500 group-hover:xl:text-accent" />
     ),
     title: 'Frontend Development',
     description:
@@ -15,7 +15,7 @@ const SERVICES_INFO = [
   },
   {
     icon: (
-      <FaMobileAlt className="text-white duration-500 group-hover:text-accent" />
+      <FaMobileAlt className="text-white duration-500 group-hover:xl:text-accent" />
     ),
     title: 'Mobile Development',
     description:
@@ -23,7 +23,7 @@ const SERVICES_INFO = [
   },
   {
     icon: (
-      <FaPalette className="text-white duration-500 group-hover:text-accent" />
+      <FaPalette className="text-white duration-500 group-hover:xl:text-accent" />
     ),
     title: 'Logo Design',
     description: 'Canvaを使いLogo制作を行えます。当サイトLogoも自作しました。',
@@ -40,46 +40,33 @@ const SERVICES_INFO = [
 
 const ServicePage = () => {
   return (
-    <section className="flex min-h-[80vh] flex-col justify-center py-12 xl:py-0">
+    <section className="flex min-h-[80vh] flex-col justify-center py-4 xl:py-0">
       <div className="container mx-auto">
         <motion.div
-          initial={{ opacity: 0 }}
+          initial={{ y: -100, opacity: 0 }}
           animate={{
+            y: 0,
             opacity: 1,
-            transition: { delay: 2.4, duration: 0.4, ease: 'linear' },
+            transition: { delay: 2.4, duration: 0.4, ease: 'easeIn' },
           }}
           className="grid grid-cols-1 gap-[40px] md:grid-cols-2"
         >
           {SERVICES_INFO.map((service, i) => (
-            <motion.div
+            <div
               key={i}
-              initial={{ y: 100, opacity: 0 }}
-              animate={{
-                y: 0,
-                opacity: 1,
-                transition: {
-                  delay: 2.4 * i + 1,
-                  duration: 0.4,
-                  ease: 'linear',
-                },
-              }}
+              className="group flex h-full flex-1 flex-col justify-center gap-6 rounded-lg border-2 border-accent p-5 transition-all duration-500 hover:xl:neon-accent"
             >
-              <div
-                key={i}
-                className="group flex h-full flex-1 flex-col justify-center gap-6 rounded-lg border-2 border-white/80 p-5 transition-all duration-500 hover:neon-accent"
-              >
-                <div className="flex w-full items-center justify-between">
-                  <div className="text-outline group-hover:text-outline-hover text-5xl font-extrabold text-transparent transition-all duration-500">
-                    {service.icon}
-                  </div>
+              <div className="flex w-full items-center justify-between">
+                <div className="text-outline text-5xl font-extrabold text-transparent transition-all duration-500">
+                  {service.icon}
                 </div>
-
-                <h2 className="text-[42px] font-bold leading-none text-white transition-all duration-500 group-hover:text-neon-accent">
-                  {service.title}
-                </h2>
-                <p className="text-white/60">{service.description}</p>
               </div>
-            </motion.div>
+
+              <h2 className="text-[42px] font-bold leading-none text-white transition-all duration-500 group-hover:xl:text-neon-accent">
+                {service.title}
+              </h2>
+              <p className="text-white/60">{service.description}</p>
+            </div>
           ))}
         </motion.div>
       </div>
