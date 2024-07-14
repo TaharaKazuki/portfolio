f63bdf2e1a586ba57640a7938510d83b236e98fc
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-07-14 10:40:57 +0900

    update

--- components/Photo.tsx
+++ components/Photo.tsx
@@ -20,7 +20,7 @@ const Photo = () => {
             opacity: 1,
             transition: { delay: 2.4, duration: 0.4, ease: 'easeInOut' },
           }}
-          className="size-[298px] mix-blend-lighten xl:size-[498px]"
+          className="absolute size-[280px] mix-blend-lighten xl:size-[480px]"
         >
           <Image
             src="/assets/photo.png"
@@ -31,6 +31,32 @@ const Photo = () => {
             className="object-contain"
           />
         </motion.div>
+        <motion.svg
+          className="size-[300px] xl:size-[506px]"
+          fill="transparent"
+          viewBox="0 0 506 506"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.circle
+            cx="253"
+            cy="253"
+            r="250"
+            stroke="#00ff99"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            initial={{ strokeDasharray: '24 10 0 0' }}
+            animate={{
+              strokeDasharray: ['15 120 25 25', '16 25 92 72', '4 250 22 22'],
+              rotate: [120, 360],
+            }}
+            transition={{
+              duration: 20,
+              repeat: Infinity,
+              repeatType: 'reverse',
+            }}
+          />
+        </motion.svg>
       </motion.div>
     </div>
   );
