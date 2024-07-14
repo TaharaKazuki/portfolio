a9dfbd17d0ba8b46527dd8b83fba10df031f2400
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-07-14 19:02:01 +0900

    update

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -6,7 +6,7 @@ import { SiTypescript } from 'react-icons/si';
 
 const SERVICES_INFO = [
   {
-    num: (
+    icon: (
       <SiTypescript className="text-white duration-500 group-hover:text-accent" />
     ),
     title: 'Frontend Development',
@@ -14,7 +14,7 @@ const SERVICES_INFO = [
       'Frontend領域の開発全般が行えます。最近はNext.js+TypeScriptを主に使用しております。',
   },
   {
-    num: (
+    icon: (
       <FaMobileAlt className="text-white duration-500 group-hover:text-accent" />
     ),
     title: 'Mobile Development',
@@ -22,14 +22,14 @@ const SERVICES_INFO = [
       'Frontend領域の延長線上として、PWA開発やReact Nativeを使ったMobile向けの開発が行えます。',
   },
   {
-    num: (
+    icon: (
       <FaPalette className="text-white duration-500 group-hover:text-accent" />
     ),
     title: 'Logo Design',
     description: 'Canvaを使いLogo制作を行えます。当サイトLogoも自作しました。',
   },
   {
-    num: (
+    icon: (
       <FaHandHoldingHeart className="text-white duration-500 group-hover:text-accent" />
     ),
     title: 'Technical Contribution',
@@ -43,31 +43,43 @@ const ServicePage = () => {
     <section className="flex min-h-[80vh] flex-col justify-center py-12 xl:py-0">
       <div className="container mx-auto">
         <motion.div
-          initial={{ y: 100, opacity: 0 }}
+          initial={{ opacity: 0 }}
           animate={{
-            y: 0,
             opacity: 1,
             transition: { delay: 2.4, duration: 0.4, ease: 'linear' },
           }}
           className="grid grid-cols-1 gap-[40px] md:grid-cols-2"
         >
           {SERVICES_INFO.map((service, i) => (
-            <div
+            <motion.div
               key={i}
-              className="group flex h-full flex-1 flex-col justify-center gap-6 rounded-lg border-2 border-white/80 p-5 transition-all duration-500 hover:neon-accent"
+              initial={{ y: 100, opacity: 0 }}
+              animate={{
+                y: 0,
+                opacity: 1,
+                transition: {
+                  delay: 2.4 * i + 1,
+                  duration: 0.4,
+                  ease: 'linear',
+                },
+              }}
             >
-              <div className="flex w-full items-center justify-between">
-                <div className="text-outline group-hover:text-outline-hover text-5xl font-extrabold text-transparent transition-all duration-500">
-                  {service.num}
+              <div
+                key={i}
+                className="group flex h-full flex-1 flex-col justify-center gap-6 rounded-lg border-2 border-white/80 p-5 transition-all duration-500 hover:neon-accent"
+              >
+                <div className="flex w-full items-center justify-between">
+                  <div className="text-outline group-hover:text-outline-hover text-5xl font-extrabold text-transparent transition-all duration-500">
+                    {service.icon}
+                  </div>
                 </div>
+
+                <h2 className="text-[42px] font-bold leading-none text-white transition-all duration-500 group-hover:text-neon-accent">
+                  {service.title}
+                </h2>
+                <p className="text-white/60">{service.description}</p>
               </div>
-
-              <h2 className="text-[42px] font-bold leading-none text-white transition-all duration-500 group-hover:text-neon-accent">
-                {service.title}
-              </h2>
-
-              <p className="text-white/60">{service.description}</p>
-            </div>
+            </motion.div>
           ))}
         </motion.div>
       </div>
