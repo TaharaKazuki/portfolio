e61cb95a84fcfa9d9a5a83617b17d056462a6b28
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-08-01 15:38:52 +0900

    update

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -358,14 +358,18 @@ const ResumePage = () => {
             >
               <div className="flex flex-col gap-[30px]">
                 <h3 className="text-4xl font-bold">{ABOUT.title}</h3>
-                <ul className="mx-auto grid max-w-[620px] grid-cols-1 gap-y-6 xl:mx-0 xl:grid-cols-2">
+                <ul className="mx-auto grid max-w-[620px] grid-cols-1 gap-y-3 xl:mx-0 xl:grid-cols-2 xl:gap-y-6">
                   {ABOUT.info.map((item, i) => (
                     <li
                       key={i}
-                      className="flex items-center justify-center gap-4 xl:justify-start"
+                      className="flex items-center justify-between gap-4 xl:justify-start"
                     >
-                      <span className="text-white/60">{item.fieldName}</span>
-                      <span className="text-xl">{item.fieldValue}</span>
+                      <span className="w-24 text-left text-white/60">
+                        {item.fieldName}
+                      </span>
+                      <div className="flex text-center text-xl">
+                        {item.fieldValue}
+                      </div>
                     </li>
                   ))}
                 </ul>
