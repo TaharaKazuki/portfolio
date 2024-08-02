6a510b9f41f3884518b6c5a3fd870d7ec8f185ff
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-08-02 10:47:50 +0900

    update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,36 @@
-import React from 'react';
+'use client';
+
+import { motion } from 'framer-motion';
+
+import { Input } from '@/components/ui/input';
 
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 3, duration: 0.4, ease: 'easeIn' },
+      }}
+      className="py-6"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-[30px] xl:flex-row">
+          <div className="order-2 xl:order-none xl:w-[54%]">
+            <form className="flex flex-col gap-6 rounded-xl bg-[#27272c] p-10">
+              <p className="text-white/60">
+                お問い合わせ・ご質問等ございましたら以下Formよりご連絡ください。
+              </p>
+
+              <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+                <Input />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
 };
 
 export default ContactPage;
