f69c716ce533a47f01dfd6c339cf12b662859be3
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-08-02 12:38:30 +0900

    update

---
+++ app/contact/_schema/index.ts
@@ -0,0 +1,20 @@
+import * as z from 'zod';
+
+const phoneRegex =
+  /^(\+?\d{1,4})?[-.\s]?(\d{2,4})[-.\s]?(\d{3,4})[-.\s]?(\d{4})$/;
+
+export const formSchema = z.object({
+  firstname: z.string().min(1, { message: '1文字以上の入力が必要です' }),
+  lastname: z.string().min(1, { message: '1文字以上の入力が必要です' }),
+  email: z
+    .string()
+    .email({ message: '正しいEmailの形式での入力をお願いします。' }),
+  phone: z
+    .string()
+    .regex(phoneRegex, { message: '正しい形式での電話番号入力をしてください' })
+    .optional(),
+  inquiry: z.enum(['web', 'mobile', 'logo', 'other']),
+  details: z.string().min(1, { message: '1文字以上の入力が必要です' }),
+});
+
+export type FormSchema = z.infer<typeof formSchema>;
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,10 @@
 'use client';
 
+import { zodResolver } from '@hookform/resolvers/zod';
 import { motion } from 'framer-motion';
+import { useForm } from 'react-hook-form';
 
+import { FormSchema, formSchema } from './_schema';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import {
@@ -16,6 +19,18 @@ import {
 import { Textarea } from '@/components/ui/textarea';
 
 const ContactPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = (data: any) => {
+    console.info(data);
+  };
+
   return (
     <motion.section
       initial={{ opacity: 0 }}
@@ -29,18 +44,60 @@ const ContactPage = () => {
         <div className="flex flex-col gap-[30px] xl:flex-row">
           {/* form */}
           <div className="mx-auto w-full">
-            <form className="flex flex-col gap-6 rounded-xl bg-[#27272c] p-10">
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="flex flex-col gap-6 rounded-xl bg-[#27272c] p-10"
+            >
               <h3 className="text-3xl text-accent">Consultation</h3>
               <p className="text-sm text-white/60">
                 ご質問・ご相談については以下フォームより承っております。
               </p>
               <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-                <Input type="firstname" placeholder="Firstname" />
-                <Input type="lastname" placeholder="Lastname" />
-                <Input type="email" placeholder="Email address" />
-                <Input type="phone" placeholder="Phone number" />
+                <Input
+                  type="text"
+                  placeholder="Firstname"
+                  {...register('firstname')}
+                />
+                {errors.firstname && (
+                  <span className="text-xs text-accent">
+                    {errors.firstname.message}
+                  </span>
+                )}
+
+                <Input
+                  type="text"
+                  placeholder="Lastname"
+                  {...register('lastname')}
+                />
+                {errors.lastname && (
+                  <span className="text-xs text-accent">
+                    {errors.lastname.message}
+                  </span>
+                )}
+
+                <Input
+                  type="email"
+                  placeholder="Email address"
+                  {...register('email')}
+                />
+                {errors.email && (
+                  <span className="text-xs text-accent">
+                    {errors.email.message}
+                  </span>
+                )}
+
+                <Input
+                  type="tel"
+                  placeholder="Phone number"
+                  {...register('phone')}
+                />
+                {errors.phone && (
+                  <span className="text-xs text-accent">
+                    {errors.phone.message}
+                  </span>
+                )}
               </div>
-              <Select>
+              <Select {...register('inquiry')}>
                 <SelectTrigger className="w-full">
                   <SelectValue placeholder="Inquiry item" />
                 </SelectTrigger>
@@ -57,6 +114,7 @@ const ContactPage = () => {
               <Textarea
                 className="h-[200px]"
                 placeholder="内容の詳細をご記載ください"
+                {...register('details')}
               />
               <div className="flex justify-center sm:justify-start">
                 <Button
