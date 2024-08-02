0170714946ecb61628c941da4f695667de561f69
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-08-02 12:12:43 +0900

    update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -28,30 +28,25 @@ const ContactPage = () => {
       <div className="container mx-auto">
         <div className="flex flex-col gap-[30px] xl:flex-row">
           {/* form */}
-          <div className="order-2 xl:order-none xl:w-[54%]">
+          <div className="mx-auto w-full">
             <form className="flex flex-col gap-6 rounded-xl bg-[#27272c] p-10">
               <h3 className="text-3xl text-accent">Consultation</h3>
-              <p className="text-white/60">
+              <p className="text-sm text-white/60">
                 ご質問・ご相談については以下フォームより承っております。
               </p>
-              {/* input */}
               <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
                 <Input type="firstname" placeholder="Firstname" />
                 <Input type="lastname" placeholder="Lastname" />
                 <Input type="email" placeholder="Email address" />
                 <Input type="phone" placeholder="Phone number" />
               </div>
-              {/* select */}
               <Select>
                 <SelectTrigger className="w-full">
-                  <SelectValue
-                    placeholder="Select a service"
-                    className="text-accent"
-                  />
+                  <SelectValue placeholder="Inquiry item" />
                 </SelectTrigger>
                 <SelectContent>
                   <SelectGroup>
-                    <SelectLabel>Select a service</SelectLabel>
+                    <SelectLabel>Inquiry item</SelectLabel>
                     <SelectItem value="web">Web Development</SelectItem>
                     <SelectItem value="mobile">Mobile Development</SelectItem>
                     <SelectItem value="logo">Logo Design</SelectItem>
@@ -59,18 +54,18 @@ const ContactPage = () => {
                   </SelectGroup>
                 </SelectContent>
               </Select>
-              {/* textarea */}
               <Textarea
                 className="h-[200px]"
                 placeholder="内容の詳細をご記載ください"
               />
-              {/* btn */}
-              <Button
-                variant={'outline'}
-                className="max-w-40 text-white xl:hover:bg-accent xl:hover:text-primary"
-              >
-                Send
-              </Button>
+              <div className="flex justify-center sm:justify-start">
+                <Button
+                  variant={'outline'}
+                  className="w-40 items-center text-white xl:hover:bg-accent xl:hover:text-primary"
+                >
+                  Send
+                </Button>
+              </div>
             </form>
           </div>
         </div>
