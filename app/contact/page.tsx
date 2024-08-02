036c8f495ad12dadc214a5890f77c06f3cce480e
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-08-02 11:09:23 +0900

    update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,7 +2,18 @@
 
 import { motion } from 'framer-motion';
 
+import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { Textarea } from '@/components/ui/textarea';
 
 const ContactPage = () => {
   return (
@@ -16,15 +27,44 @@ const ContactPage = () => {
     >
       <div className="container mx-auto">
         <div className="flex flex-col gap-[30px] xl:flex-row">
+          {/* form */}
           <div className="order-2 xl:order-none xl:w-[54%]">
             <form className="flex flex-col gap-6 rounded-xl bg-[#27272c] p-10">
+              <h3 className="text-4xl text-accent">ご質問・ご相談</h3>
               <p className="text-white/60">
-                お問い合わせ・ご質問等ございましたら以下Formよりご連絡ください。
+                ご質問・ご相談については以下、フォームより承ります。
               </p>
-
+              {/* input */}
               <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-                <Input />
+                <Input type="firstname" placeholder="氏" />
+                <Input type="lastname" placeholder="名" />
+                <Input type="email" placeholder="Email address" />
+                <Input type="phone" placeholder="Phone number" />
               </div>
+              {/* select */}
+              <Select>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select a service" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Select a service</SelectLabel>
+                    <SelectItem value="web">Web Development</SelectItem>
+                    <SelectItem value="mobile">Mobile Development</SelectItem>
+                    <SelectItem value="logo">Logo Design</SelectItem>
+                    <SelectItem value="other">other</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              {/* textarea */}
+              <Textarea
+                className="h-[200px]"
+                placeholder="詳細を記載してください"
+              />
+              {/* btn */}
+              <Button size="md" className="max-w-40">
+                Send
+              </Button>
             </form>
           </div>
         </div>
